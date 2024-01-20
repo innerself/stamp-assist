596d7d98935ac47f4a47cad1a935c138bf1a4af2
Sergey Petrin <spetrin@gmail.com>
2024-01-20 22:47:52 +0300

    Add logging

--- stamp_assist/settings.py
+++ stamp_assist/settings.py
@@ -100,6 +100,20 @@ DATABASES = {
     },
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
